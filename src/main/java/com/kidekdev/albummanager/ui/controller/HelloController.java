c47c25c8a35a97a9ec9c9f52f841f637f9a84034
Dmitry <KideDev@gmail.com>
2025-11-06 15:39:02 +0300

    Init

--- src/main/java/com/kidekdev/albummanager/ui/controller/HelloController.java
+++ src/main/java/com/kidekdev/albummanager/ui/controller/HelloController.java
@@ -1,7 +1,13 @@
 package com.kidekdev.albummanager.ui.controller;
 
+import com.kidekdev.albummanager.database.loader.DatabaseLoader;
+import com.kidekdev.albummanager.database.service.DatabaseService;
+import com.kidekdev.albummanager.database.service.InMemoryDatabaseService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HelloController {
     @FXML
@@ -9,6 +15,9 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        DatabaseLoader loader = new DatabaseLoader();
+        Path path = Paths.get("C:\\Users\\Kidek\\Desktop\\metadata");
+        DatabaseService db = new InMemoryDatabaseService(loader.loadDatabase(path));
+        welcomeText.setText(db.getAllTracks().get(0).getDescription());
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,4 +11,28 @@ module com.kidekdev.albummanager {
 
     exports com.kidekdev.albummanager.ui.controller;
     opens com.kidekdev.albummanager.ui.controller to javafx.fxml;
+
+    exports com.kidekdev.albummanager.database.loader;
+    opens com.kidekdev.albummanager.database.loader to javafx.fxml;
+
+    exports com.kidekdev.albummanager.database.model.album;
+    exports com.kidekdev.albummanager.database.model.common;
+    exports com.kidekdev.albummanager.database.model.folder;
+    exports com.kidekdev.albummanager.database.model.journal;
+    exports com.kidekdev.albummanager.database.model.project;
+    exports com.kidekdev.albummanager.database.model.resource;
+    exports com.kidekdev.albummanager.database.model.tag;
+    exports com.kidekdev.albummanager.database.model.view;
+
+    opens com.kidekdev.albummanager.database.model.album to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.common to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.folder to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.journal to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.project to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.resource to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.tag to javafx.fxml;
+    opens com.kidekdev.albummanager.database.model.view to javafx.fxml;
+
+    exports com.kidekdev.albummanager.database.service;
+    opens com.kidekdev.albummanager.database.service to javafx.fxml;
 }
